ea0e4c96951e76cf034b35c4f0282d12ad483d26
unknown <jgraves678@insite.4cd.edu>
2024-10-06 05:10:46 -0700

    10-minute update

---
+++ 210-lab-18.cpp
@@ -0,0 +1,43 @@
+//Write a C++ program which could be used to efficiently store these reviews. Implement this in a list.
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Create a node for reviews
+
+
+// write code that adds nodes to the tail of the linked list. The sample code in this module contains the code to add notes to the head of the linked list.//
+struct Review {
+    double rating;  //data
+    string comments;
+    Review *next;     //ptrs
+}
+
+
+int main () {
+    Review* head nullptr;
+    int choice;
+    char continueInput;
+    double rating;
+    string comments;
+
+//At program startup, ask the user which mode to use, new nodes at the head or the tail of the linked list.//
+cout << "Which linked list method should we use?\n";
+cout<< " [1] New nodes are added at the head of the linked list\n";
+cout<< " [2] New nodes are added at the tail of the linked list.\n";
+cin>> "choice: ";
+cin.ignore();
+
+
+//Obtain two pieces of data from the user, the rating and the comments. Store these in the linked list.//
+
+
+//ask user for rating ranging from 0 - 5, rating data type is double
+
+//ask user if they have any comments stored in linked list
+
+//ask user if they have another review, yes or no question
+
+//display all reviews, review #:, rating:, traversing a linked list
+return 0;
+}
